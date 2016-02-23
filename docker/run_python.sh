460b5f1bcdbd806b6434e1a31d196d21d5c322d3
fiona <fiona@basistech.com>
2016-02-23 14:37:42 -0500

    Fixed indentation issue in Dockerfile

--- docker/run_python.sh
+++ docker/run_python.sh
@@ -3,17 +3,15 @@
 #Gets called when the user doesn't provide any args
 function HELP {
     echo -e "\nusage: source_file.py --key API_KEY [--url ALT_URL]"
-    echo "  API_KEY       - Rosette API key (required)"
-    echo "  FILENAME      - Python source file (optional)"
-    echo "  ALT_URL       - Alternate service URL (optional)"
-    echo "  GIT_USERNAME  - Git username where you would like to push regenerated gh-pages (optional)"
-    echo "  VERSION       - Build version (optional)"
-    echo "Compiles and runs the source file(s) using the local development source."
+    echo "  API_KEY      - Rosette API key (required)"
+    echo "  FILENAME     - Python source file (optional)"
+    echo "  ALT_URL      - Alternate service URL (optional)"
+    echo "Compiles and runs the source file(s) using the published rosette-api"
     exit 1
 }
 
 #Gets API_KEY, FILENAME and ALT_URL if present
-while getopts ":API_KEY:FILENAME:ALT_URL:GIT_USERNAME:VERSION" arg; do
+while getopts ":API_KEY:FILENAME:ALT_URL" arg; do
     case "${arg}" in
         API_KEY)
             API_KEY=${OPTARG}
@@ -25,14 +23,6 @@ while getopts ":API_KEY:FILENAME:ALT_URL:GIT_USERNAME:VERSION" arg; do
             ;;
         FILENAME)
             FILENAME=${OPTARG}
-            usage
-            ;;
-        GIT_USERNAME)
-            GIT_USERNAME=${OPTARG}
-            usage
-            ;;
-        VERSION)
-            VERSION={OPTARG}
             usage
             ;;
     esac
@@ -48,46 +38,25 @@ function checkAPI {
 }
 
 #Copy the mounted content in /source to current WORKDIR
-cp -r -n /source/* .
+cp -r /source/* .
 
 #Run the examples
 if [ ! -z ${API_KEY} ]; then
     checkAPI
     #Prerequisite
-    python /python-dev/setup.py install
-    cd /python-dev/examples
+    python /python/setup.py install
+    cd examples
     if [ ! -z ${FILENAME} ]; then
         if [ ! -z ${ALT_URL} ]; then
-	       python ${FILENAME} --key ${API_KEY} --url ${ALT_URL} 
-        else
-	       python ${FILENAME} --key ${API_KEY} 
-   	    fi
+	    python ${FILENAME} --key ${API_KEY} --url ${ALT_URL} 
+	else
+	    python ${FILENAME} --key ${API_KEY} 
+   	fi
     elif [ ! -z ${ALT_URL} ]; then
-        find -maxdepth 1  -name '*.py' -print -exec python {} --key ${API_KEY} --url ${ALT_URL} \;
+    	find -maxdepth 1  -name '*.py' -print -exec python {} --key ${API_KEY} --url ${ALT_URL} \;
     else
-        find -maxdepth 1  -name '*.py' -print -exec python {} --key ${API_KEY} \;
+	find -maxdepth 1  -name '*.py' -print -exec python {} --key ${API_KEY} \;
     fi
 else 
     HELP
-fi
-
-#Run unit tests
-cd /python-dev
-tox
-
-#Generate gh-pages and push them to git account (if git username is provided)
-if [ ! -z ${GIT_USERNAME} ] && [ ! -z ${VERSION} ]; then
-    #clone python git repo
-    cd /
-    git clone git@github.com:${GIT_USERNAME}/python.git
-    cd python
-    git checkout origin/gh-pages -b gh-pages
-    git branch -d develop
-    #generate gh-pages and set ouput dir to git repo (gh-pages branch)
-    cd /python-dev
-    .tox/py27/bin/epydoc -v --no-private --no-frames --css epydoc.css -o /python rosette/*.py
-    cd /python
-    git add .
-    git commit -a -m "publish python apidocs ${VERSION}"
-    git push
 fi
