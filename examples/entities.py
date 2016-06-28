ea08c8e86f9ce45b2f92ec4c818de3a7106878be
fiona <fiona@basistech.com>
2016-06-28 15:57:49 -0400

    Style fix

--- examples/entities.py
+++ examples/entities.py
@@ -18,7 +18,7 @@ def run(key, altUrl='https://api.rosette.com/rest/v1/'):
     params = DocumentParameters()
     params["content"] = entities_text_data
     params["genre"] = "social-media"
-    return api.entities(params) 
+    return api.entities(params)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='Calls the ' + os.path.splitext(os.path.basename(__file__))[0] + ' endpoint')
 parser.add_argument('-k', '--key', help='Rosette API Key', required=True)
