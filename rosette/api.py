1874e6ac39344734464749a259d914f4dc6413ca
Chris Park <cpark@basistech.com>
2016-11-18 13:15:35 +0000

    Version 1.4.2

--- rosette/api.py
+++ rosette/api.py
@@ -28,7 +28,7 @@ import requests
 import re
 import warnings
 
-_BINDING_VERSION = '1.4.1'
+_BINDING_VERSION = '1.4.2'
 _GZIP_BYTEARRAY = bytearray([0x1F, 0x8b, 0x08])
 
 _IsPy3 = sys.version_info[0] == 3
