8a510bbda347790d180937d953c7ae096b5c4fc0
Ervin Papp <epapp@basistech.com>
2023-06-27 12:13:48 +0200

    RCB-601 new: events example

---
+++ examples/events.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Example code to call Rosette API to get entities from a piece of text.
+"""
+
+import argparse
+import json
+import os
+
+from rosette.api import API, DocumentParameters, RosetteException
+
+
+def run(key, alt_url='https://api.rosette.com/rest/v1/'):
+    """ Run the example """
+    # Create an API instance
+    api = API(user_key=key, service_url=alt_url)
+
+    # Set selected API options.
+    # For more information on the functionality of these
+    # and other available options, see Rosette Features & Functions
+    # https://developer.rosette.com/features-and-functions#entity-extraction-and-linking
+
+    # api.set_option('calculateSalience','true')
+    # api.set_option('linkEntities','false')
+
+    events_text_data = "James wanted to test this awesome document."
+    params = DocumentParameters()
+    params["content"] = events_text_data
+
+    try:
+        return api.events(params)
+    except RosetteException as exception:
+        print(exception)
+
+PARSER = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description='Calls the ' +
+                                 os.path.splitext(os.path.basename(__file__))[0] + ' endpoint')
+PARSER.add_argument('-k', '--key', help='Rosette API Key', required=True)
+PARSER.add_argument('-u', '--url', help="Alternative API URL",
+                    default='https://api.rosette.com/rest/v1/')
+
+if __name__ == '__main__':
+    ARGS = PARSER.parse_args()
+    RESULT = run(ARGS.key, ARGS.url)
+    print(RESULT)
