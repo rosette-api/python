97bf901906e13402292972f5a9ee1163e31a2597
Adam Soos <asoos@basistech.com>
2023-12-05 11:33:50 +0100

    WS_3053: revert url

--- examples/categories.py
+++ examples/categories.py
@@ -12,7 +12,7 @@ import os
 from rosette.api import API, DocumentParameters, RosetteException
 
 
-def run(key, alt_url='http://localhost:8181/rest/v1/'):
+def run(key, alt_url='https://api.rosette.com/rest/v1/'):
     """ Run the example """
     categories_text_data = "If you are a fan of the British television series Downton Abbey and you are planning to be in New York anytime before April 2nd, there is a perfect stop for you while in town."
     # Create an API instance
