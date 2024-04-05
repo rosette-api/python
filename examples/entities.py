0c93c5b2c7c3f4c0decdc0e1a6d17468538d086c
Adam Soos <asoos@basistech.com>
2024-04-05 17:14:02 +0200

    WS-3151: add coref option example

--- examples/entities.py
+++ examples/entities.py
@@ -22,6 +22,7 @@ def run(key, alt_url='https://api.rosette.com/rest/v1/'):
 
     # api.set_option('calculateSalience','true')
     # api.set_option('linkEntities','false')
+    # api.set_option('useIndocServer', True)
 
     entities_text_data = "The Securities and Exchange Commission today announced the leadership of the agency’s trial unit.  Bridget Fitzpatrick has been named Chief Litigation Counsel of the SEC and David Gottesman will continue to serve as the agency’s Deputy Chief Litigation Counsel. Since December 2016, Ms. Fitzpatrick and Mr. Gottesman have served as Co-Acting Chief Litigation Counsel.  In that role, they were jointly responsible for supervising the trial unit at the agency’s Washington D.C. headquarters as well as coordinating with litigators in the SEC’s 11 regional offices around the country."
     params = DocumentParameters()
