db7e3a1adb8616d82e30356de68dbf2b9ea511e6
Adam Soos <asoos@basistech.com>
2024-11-04 11:38:37 +0100

    WS-3314: fix example runner

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -4,12 +4,12 @@ if [ $# -eq 0 ]; then
     echo "Usage: $0 API_KEY [ALT_URL]" 1>&2
     exit 1
 fi
-OPTS=""
-if [ -n "$2" ]; then
-    OPTS="-u $2"
-fi
 
 for f in *.py
 do
-    python $f --key $1 "$OPTS"
+  if [ -n "$2" ]; then
+    python $f --key $1 --url $2
+  else
+    python $f --key $1
+  fi
 done
